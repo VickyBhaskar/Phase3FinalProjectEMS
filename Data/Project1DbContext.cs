f7a7bc5e483ec511e823a463546d33e4f7c0d8ee
VickyBhaskar <vicky2400kumar@gmail.com>
2024-01-03 21:09:13 +0530

    initial Commit

---
+++ Data/Project1DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project1Phase3.Models;
+
+namespace Project1Phase3.Data
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext (DbContextOptions<ProjectDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project1Phase3.Models.EmpProfile> EmpProfile { get; set; } = default!;
+
+        public DbSet<Project1Phase3.Models.DeptMaster>? DeptMaster { get; set; }
+    }
+}
---
+++ Models/EmpProfile.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project1Phase3.Models
+{
+    [Table("EmpProfile")]
+    public class EmpProfile
+    {
+        [Key]
+        public int EmpCode { get; set; }
+        public string? EmpName { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Email { get; set;}
+       
+    }
+}
